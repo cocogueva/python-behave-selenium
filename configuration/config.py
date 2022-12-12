ca590e22c015d3b00a6c92bb3be6c1b6a20eb172
Jorge Guevara Lujan <jguevara@growthaccelerationpartners.com>
2022-12-12 17:10:06 -0500

    Initial commit: seting framework

---
+++ configuration/config.py
@@ -0,0 +1,10 @@
+class TestData:
+    BROWSER = 'chrome'
+    CHROME_EXECUTABLE_PATH = "drivers/chromedriver"
+    FIREFOX_EXECUTABLE_PATH = "Drivers/geckodriver"
+
+    URL = "https://www.saucedemo.com//"
+    USERNAME = "standard_user"
+    PASSWORD = "secret_sauce"
+
+    IMPLICIT_WAIT = 10
